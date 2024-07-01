db224831ac3b5ba82181562d32116389b2d84aaa
SnakeEye <abdallah.s_ah@outlook.com>
2024-07-01 13:27:47 +0300

    first commit

---
+++ src/API/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCgAn3GnpjoZ8J_uWa7207-E0qWxQDJqqk",
+  authDomain: "fullstack-submit.firebaseapp.com",
+  projectId: "fullstack-submit",
+  storageBucket: "fullstack-submit.appspot.com",
+  messagingSenderId: "10199574585",
+  appId: "1:10199574585:web:864bbaac6e2fdd214bc5e8",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
